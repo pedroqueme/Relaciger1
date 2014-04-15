27d688290ba2813ded530922eca6f030b71b479b
Pedro Jose Queme Peña <pedroqueme@gmail.com>
2014-04-15 17:33:47 -0500

    Changes made by Pedro Jose Queme Peña (pedroqueme@gmail.com).

--- Relaciger/scripts/app.js
+++ Relaciger/scripts/app.js
@@ -7,17 +7,5 @@
     }, false);
 
     app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
+    
 })(window);
